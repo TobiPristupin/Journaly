9af259c2070dfd91c65768807549b420b7f853b0
Tobias Pristupin <tobipristupin@fb.com>
2021-07-12 14:13:23 -0400

    - Created UI for login screen - Designed a color theme for app

--- app/src/main/java/com/example/journaly/MainActivity.java
+++ app/src/main/java/com/example/journaly/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.journaly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.journaly.databinding.ActivityMainBinding;
+import com.example.journaly.login.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        goToLogin();
+        setContentView(binding.getRoot());
+    }
+
+    private void goToLogin(){
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        //Flags prevent user from returning to MainActivityView when pressing back button
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/journaly/login/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.journaly.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.journaly.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
